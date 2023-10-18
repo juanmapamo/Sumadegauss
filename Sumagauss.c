1435838dcee45e797daaf3537b7b62ab939ab7f1
juanmapamo <139057889+juanmapamo@users.noreply.github.com>
2023-10-18 15:35:16 -0500

    Update Sumagauss.c

--- Sumagauss.c
+++ Sumagauss.c
@@ -2,18 +2,22 @@
 int main ()
 {
     int numeroentero;
-    int suma;
+    int suma=0;
     printf("En este programa podras saber el resultado de una suma de gauss\n");
-    printf("Ingrese hasta que numero desea que se sume\n");
+    printf("Ingrese hasta que numero desea que se sume, debe ser positivo\n");
     scanf("%d", &numeroentero);
     if (numeroentero>0)
     {
-        for (size_t i = 1; i < numeroentero; i++)
+        for (int i = 1; i <= numeroentero; i++)
         {
             suma=suma+i;
         }
-        
+        printf ("El resultado de la suma de Gauss %d es %d\n", &numeroentero, &suma);  
     }
-    printf ("El resultado de la suma es %d\n", &suma);
+    else 
+    {
+        printf ("Solo deben ser numeros positivos");
+    }
+    
     return 0;
 }
